c81cab9977c49d2eb4bcea12840bb9f449441112
Junhong-Kim <jhk1005jhk@gmail.com>
2021-04-17 21:44:15 +0900

    init

---
+++ src/test/java/com/example/expawsfargate/ExpAwsFargateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expawsfargate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpAwsFargateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
